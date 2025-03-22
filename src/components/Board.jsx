7106c1fa1c109948aafc61d700011c8a500f9c8d
IWToIT <matylitski.egor.99@gmail.com>
2025-03-22 21:42:35 +0300

    feat: next Tentramino display & Score counter

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import Cell from "./Cell";
 
-function Board({ board }) {
+const Board = ({ board }) => {
   return (
     <div className="board">
-      {board.map((row, y) =>
-        row.map((cell, x) => <Cell key={`${y}-${x}`} type={cell} />)
-      )}
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {row.map((cell, cellIndex) => (
+            <div
+              key={cellIndex}
+              className={`board-cell ${cell !== "0" ? "filled" : ""}`}
+              style={{
+                backgroundColor:
+                  cell !== "0" && cell.color
+                    ? `rgb(${cell.color})`
+                    : "transparent",
+              }}
+            ></div>
+          ))}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default Board;
