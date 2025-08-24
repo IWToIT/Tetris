a31b624a01a7601192eb1aa918f48af0e4bc04f3
Егор <matylitski.egor.99@gmail.com>
2025-08-24 12:59:35 +0300

    latest

--- src/components/NextTetramino.jsx
+++ src/components/NextTetramino.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NextTetramino = ({ shape, color }) => {
   return (
     <div className="next-tetramino">
-      <h3>Next Tetramino</h3>
+      <span>Next Tetramino</span>
       <div className="next-tetramino-grid">
         {shape.map((row, rowIndex) => (
           <div key={rowIndex} className="next-tetramino-row">
